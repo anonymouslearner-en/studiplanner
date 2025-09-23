e3060ca3dba6453c767f04cf8395da2c2a2f2cd8
anonymouslearner <anonymouslearner-en@github.com>
2025-09-23 12:13:50 +0200

    feat: :label: add types

---
+++ src/data/types.ts
@@ -0,0 +1,45 @@
+type tCompleted = "succeeded" | "failed";
+
+interface MetaData {
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+/* ==================================
+                EXAM
+===================================== */
+
+//ToDo better name
+interface ExamData {
+  scheduledDate: Date;
+  scheduledPlace: string;
+  duration?: number; // in minutes
+  notes?: string;
+}
+
+export interface Topic {
+  name: string;
+  id: string;
+  state: "pending" | "learning" | "reviewing" | "exam questions";
+  actions: null;
+  difficultyLevel: "easy" | "moderate" | "diffucult" | "endgame";
+  relevance: "explicitly not coming" | "could appear" | "focus";
+}
+
+export interface Exam {
+  name: string;
+  id: string;
+  state: "pending" | "ongoing" | tCompleted;
+  actionFlags: "forget it" | "work hard" | "good";
+  details: ExamData;
+  topics: Topic[];
+  metadata: MetaData;
+}
+
+/* ==================================
+                LAB
+===================================== */
+export interface Lab {
+  name: string;
+  state: "pending" | "";
+}
