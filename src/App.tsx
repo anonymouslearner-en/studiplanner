33a396499a9cb584c326a9f8521c3d3131dcdff0
anonymouslearner-en <whateverittakescm+uni@gmail.com>
2025-10-15 20:22:07 +0200

    refactor: :recycle: minor refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import { OverviewSection } from "./components/organisms/OverviewSection";
 import { mockExams } from "./mocks/mock";
 import { Page } from "./components/layout/Page";
 
-function App() {
+const App = () => {
   return (
     <Page header={<Navbar />} footer={<Footer />}>
-      <div className="w-full max-w-5xl mx-auto pt-5 px-4 mb-28">
+      <section className="w-full max-w-5xl mx-auto pt-5 px-4 mb-28">
         <h1 className="text-white font-medium text-base mb-4">Hello</h1>
         <ComingUpSection />
         <OverviewSection exams={mockExams} />
-      </div>
+      </section>
     </Page>
   );
-}
+};
 
 export default App;
