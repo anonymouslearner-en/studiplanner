32e46c6c1bec56ed43237970dc5aa5bbf7374f58
anonymouslearner-en <whateverittakescm+uni@gmail.com>
2025-11-18 23:34:22 +0100

    refactor: :truck: minor refactoring

---
+++ src/forms/OnboardingForm.tsx
@@ -0,0 +1,23 @@
+/**
+ * @file OnboardingForm
+ *
+ * @author Clinton Mzee
+ */
+
+// export const OnboardingForm: FC
+
+// STEP ONE
+// Select School (TH Köln) and faculty
+// If Not exit (and go to default)
+
+// STEP TWO
+// Select Programme and Semester
+// If Not exit (and go to default)
+
+// STEP THREE
+// Choose Exams Taking Programme and Semester
+// If Not exit (and go to default)
+
+// STEP FOUR (Optional)
+// Select Retakes or Antizyklische Exams
+// If Not exit (and take current settings)!
